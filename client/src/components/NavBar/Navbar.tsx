bc3d28011e4e7bc30cb6a02a43b2a5faaf4dcbb6
Leonard <leonard@Leonards-MacBook-Pro-2.local>
2023-05-29 23:26:45 -0700

    added hamburger functionality on mobile

--- client/src/components/NavBar/Navbar.tsx
+++ client/src/components/NavBar/Navbar.tsx
@@ -1,23 +1,31 @@
 import quellLogo from '/client/src/assets/images/quell_logos/quell-logo-side.svg';
-import React, { useState } from 'react';
+import hamburgerIcon from '/client/src/assets/images/graphics/hamburger.svg';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import { HashLink } from 'react-router-hash-link'; 
 
 export const Navbar = () => {
 
-  // state for checking if if the hamburger is open/closed
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  // function to toggle the menu upon click
+  // State for checking if if the hamburger is open/closed
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu upon click
   const toggleMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
+    setIsMenuOpen(!isMenuOpen);
   };
+
+  // Function close menu when clicking on nav bar item
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
 
   return (
     <nav className="relative container mx-auto bg-background w-full p-8 text-white xl:max-w-10xl">
       <div className="flex items-center justify-between">
         <div className="pt-2">
-          <Link to="/">
-          <img className="bird-icon" src={quellLogo} />
+          <Link to="/" onClick={closeMenu}>
+            <img className="bird-icon" src={quellLogo} alt="Quell Logo" />
           </Link>
         </div>
         <div className="hidden font-sans font-light space-x-12 md:flex">
@@ -26,18 +34,16 @@ export const Navbar = () => {
           <a href="#" className="hover:underline underline-offset-8 decoration-lightblue">Blog</a>
           <a href="#" className="hover:underline underline-offset-8 decoration-lightblue">Try Demo</a>
         </div>
-       <button id="menu-btn" className="block hambuger md:hidden focus:outline-none">
-          <span className='hamburger-top'></span>
-          <span className='hamburger-middle'></span>
-          <span className='hamburger-bottom'></span>
-        </button>  
+        <div className="block cursor-pointer md:hidden" onClick={toggleMenu}>
+          <img className="w-4 h-auto" src={hamburgerIcon} alt="Hamburger Icon" />
+        </div>
       </div>
-      <div className="md:hidden">
-        <div id="menu" className="absolute flex flex-col items-center hidden self-end py-8 mt-10 space-y-6 bg-background sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
-          <a href='#'>Docs</a>
-          <a href='#'>Team</a>
-          <a href='#'>Blog</a>
-          <a href='#'>Try Demo</a>
+      <div className={`md:hidden ${isMenuOpen ? '' : 'hidden'}`}>
+        <div id="menu" className={`absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 bg-background sm:w-auto sm:self-center left-6 right-6 drop-shadow-md ${isMenuOpen ? '' : 'hidden'}`} style={{ zIndex: 9999 }}>
+          <a href='#' className="hover:underline underline-offset-8 decoration-lightblue">Docs</a>
+          <Link to="/team" onClick={closeMenu}><a href='#' className="hover:underline underline-offset-8 decoration-lightblue">Team</a></Link>
+          <a href='#' className="hover:underline underline-offset-8 decoration-lightblue">Blog</a>
+          <a href='#' className="hover:underline underline-offset-8 decoration-lightblue">Try Demo</a>
         </div>
       </div>
     </nav>
