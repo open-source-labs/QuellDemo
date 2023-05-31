24b33aa20fffd7be8152a75f5f012da06a6a2bca
Justin Hua <justinfhua@gmail.com>
2023-05-31 14:21:37 -0700

    added links to medium and extension

--- client/src/components/Feature-Callouts/Feature-Callouts.tsx
+++ client/src/components/Feature-Callouts/Feature-Callouts.tsx
@@ -20,7 +20,7 @@ export const FeatureCallouts = () => {
                         <h1 className="leading-snug text-2xl font-sans font-semibold text-lightblue mb-4 lg:text-3xl">New Feature 🔥</h1>
                         <h1 className="leading-snug text-3xl font-sans font-semibold text-white mb-4 lg:text-4xl ">Context Visualizer</h1>
                         <p className="font-sans font-extralight text-white mb-6 lg:text-xl">Use Quell's new context visualizer to better understand the relationship between a query and its GraphQL structure, view response speeds on each field, and get data for more effective debugging.</p>
-                        <button className="bg-transparent border border-lightblue text-lightblue font-sans py-3 px-6 rounded hover:bg-lightblue hover:text-white md:w-1/2 xl:text-xl">Learn more</button>
+                        <button className="bg-transparent border border-lightblue text-lightblue font-sans py-3 px-6 rounded hover:bg-lightblue hover:text-white md:w-1/2 xl:text-xl" onClick={() => { window.location.href = 'https://medium.com/@quellcache/query-without-worry-quell-8-0-launches-to-amplify-graphql-queries-35448c694e4f'; } }>Learn more</button>
                       </div>
                     </div>
                   <div className="pt-4 md:self-start md:pt-6 lg:pt-0 xl:pl-12 xl:ml-12">
@@ -44,7 +44,7 @@ export const FeatureCallouts = () => {
                   <div className="flex flex-col mb-6 xl:w-3/4">
                     <h1 className="leading-snug text-3xl font-sans font-semibold text-white mb-4 lg:text-4xl ">Get the Chrome Extension</h1>
                     <p className="font-sans font-extralight text-white mb-6 lg:text-xl">Download the Quell Chrome Extension to get real-time feedback on your GraphQL app and access all the developer tools that Quell has to offer.</p>
-                    <button className="bg-transparent border border-lightblue text-lightblue font-sans py-3 px-6 rounded hover:bg-lightblue hover:text-white md:w-1/2 xl:text-xl">Download Now</button>
+                    <button className="bg-transparent border border-lightblue text-lightblue font-sans py-3 px-6 rounded hover:bg-lightblue hover:text-white md:w-1/2 xl:text-xl" onClick={() => { window.location.href = 'https://chrome.google.com/webstore/detail/quell-developer-tool/jnegkegcgpgfomoolnjjkmkippoellod'; } }>Download Now</button>
                   </div>
                 </div>
               </div>
