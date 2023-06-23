efc1955ac25d97866949f4ad0da01c96cae1f557
Andrew Dai <andrewdai@andrews-mbp.lan>
2023-06-22 19:35:50 -0500

    added more TS to testSchema files

--- quell-server/test-config/countriesModel.ts
+++ quell-server/test-config/countriesModel.ts
@@ -1,4 +1,3 @@
-
 import { Pool } from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -10,9 +9,22 @@ const pool = new Pool({
   connectionString: URI
 });
 
+type Name = {
+  name: string
+}
 
 export default {
   query: (text: string, params?: (number | string)[] ) => {
     return pool.query(text, params);
+  },
+  // Below are placeholders, need to research more into the Pool methods
+  create: (text: Name, params?: (number | string)[] ) => {
+    return pool.query(text.name, params);
+  },
+  findOne: (text: Name, params?: (number | string)[] ) => {
+    return pool.query(text.name, params);
+  },
+  deleteOne: (text: Name, params?: (number | string)[] ) => {
+    return pool.query(text.name, params);
   }
 };
