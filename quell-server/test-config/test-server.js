20fcf1aa89a0f434a08f54b9e22f8d180ca865fe
staceyjhlee <staceyjlee18@gmail.com>
2023-07-01 11:12:01 -0400

    modularized-graphs

--- quell-server/test-config/test-server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-// const express = require('express');
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-module.exports = app;
